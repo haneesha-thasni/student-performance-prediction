0a83db276e0fe05a63dd62dfae4579335f18ef90
Haneesha Thasni N <haneeshathasni8@gmail.com>
2025-02-20 13:37:23 +0530

    Update student_performance_app.py

--- student_performance_app.py
+++ student_performance_app.py
@@ -11,7 +11,7 @@ st.title("Student Performance Prediction")
 st.write("A web app to predict student performance for better learning outcome!")
 
 hours_studied=st.number_input("How many hours did the student study per day?",max_value=24)
-previous_score=st.number_input("Enter the student's past academic score")
+previous_score=st.number_input("Enter the student's past academic score",value=0,placeholder=" ")
 sleep_hours=st.number_input("How many hours does the student sleep?")
 sample_questions_practiced=st.number_input("How many sample question papers has the student practiced?")
 extra_curricular_activities=st.selectbox('Has the student been involved in any extra curricular activites?',('Yes','No'))
